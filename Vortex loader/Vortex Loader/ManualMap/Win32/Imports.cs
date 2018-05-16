d30d1e2fbd2552dafb2bae8ec2ab1268b528aeb5
Kan <38980663+Kanepu@users.noreply.github.com>
2018-05-16 09:19:08 +0400

    Update Imports.cs

--- Vortex loader/Vortex Loader/ManualMap/Win32/Imports.cs
+++ Vortex loader/Vortex Loader/ManualMap/Win32/Imports.cs
@@ -171,16 +171,3 @@ namespace Vortex_Loader.ManualMap.Win32
         }
     }
 }
-
-//-----------------------------------------------------
-// Coded by Kanepu! Vortex loader source
-// https://github.com/Kanepu/Vortex-loader
-// Note to the person using this, removing this
-// text is in violation of the license you agreed
-// to by downloading. Only you can see this so what
-// does it matter anyways.
-// Copyright © Kanepu 2018
-// Licensed under a MIT license
-// Read the terms of the license here
-// https://github.com/Kanepu/Vortex-loader/blob/master/LICENSE
-//-----------------------------------------------------
